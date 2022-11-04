f5f67f4e3943014e0484bfcbe3d36294f78768a1
Lindomar <lindomarfarineli@hotmail.com>
2022-11-04 00:12:56 -0300

    segundo

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var state = {board: []};
+
+function start() {
+    renderBoard();
+    renderButton();
+};
+
+function renderBoard() {
+
+    let ramdomNumber =  Math.ceil(Math.random()*26);
+    if (ramdomNumber === 0 ) {
+        ramdomNumber = 1;
+    }
+    
+    way = 'img/' + ramdomNumber.toString() + '.jpg';
+    var divBoard = document.querySelector('#image-board')
+    divBoard.innerHTML = '';
+    let fotho = document.createElement('img');
+    fotho.src = way;
+    divBoard.append(fotho);
+}
+
+function renderButton(){
+    var divButton = document.querySelector('#update-button')
+    divButton.innerHTML = '';
+    let buttonUpdate = document.createElement('button');
+    buttonUpdate.textContent = 'Nova Imagem';
+    buttonUpdate.addEventListener('click', atualize);
+    divButton.append(buttonUpdate);
+}
+
+function atualize(event){
+    start();
+}
+
+start();
