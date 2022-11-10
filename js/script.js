0ca06b747e3a70827795bc7fd6a2a265796bfcaf
Leo Falco <leonardo.falco@outlook.com>
2022-11-10 19:20:04 -0300

    chore: melhorias nos estilos

--- js/script.js
+++ js/script.js
@@ -1,36 +1,19 @@
-var state = {board: []};
+const imageWrapper = document.querySelector('#card')
 
-function start() {
-    renderBoard();
-    renderButton();
-};
-
-function renderBoard() {
-
-    let ramdomNumber =  Math.ceil(Math.random()*27);
-    if (ramdomNumber === 0 ) {
-        ramdomNumber = 1;
-    }
-    
-    way = 'img/' + ramdomNumber.toString() + '.png';
-    var divBoard = document.querySelector('#image-board')
-    divBoard.innerHTML = '';
-    let fotho = document.createElement('img');
-    fotho.src = way;
-    divBoard.append(fotho);
+function drawNewCard () {
+  imageWrapper.childNodes.forEach(child => child.remove())
+  imageWrapper.append(getRandomCardImage());
 }
 
-function renderButton(){
-    var divButton = document.querySelector('#update-button')
-    divButton.innerHTML = '';
-    let buttonUpdate = document.createElement('button');
-    buttonUpdate.textContent = 'Nova Imagem';
-    buttonUpdate.addEventListener('click', atualize);
-    divButton.append(buttonUpdate);
+function getRandomCardImage () {
+  const img = document.createElement('img');
+  const random = randomIntegerBetween(1, 27)
+  img.src = `img/${random}.png`
+  return img
 }
 
-function atualize(event){
-    start();
+function randomIntegerBetween (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-start();
+drawNewCard();
